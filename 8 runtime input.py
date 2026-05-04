b246bb02b5837498efc3a4b9980238c288f63d41
Mogalapalli Lokesh Gupta <90005355+lokeshdot23@users.noreply.github.com>
2026-05-04 14:30:52 +0530

    Add files via upload

---
+++ 8 runtime input.py
@@ -0,0 +1,137 @@
+
+'''a=10
+b=20
+print(a+b)
+a=2;b=5
+c=a+b
+print(c)'''
+#print(5+9)
+
+# run time input()
+'''
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+print(a+b)
+'''
+'''
+a=float(input("enter a val in float"))
+b=float(input("enter b val in float"))
+print(a+b)
+'''
+'''
+a=str(input("enter a string"))
+b=str(input("enter b string"))
+print((a+" " +b).title())
+'''
+'''
+a=input("fname")
+b=input("lname")
+print((a+" "+b).title())
+'''
+'''
+city=input("city name")
+print('this city name is',city)
+'''
+'''
+a=complex(input("entre a complex"))
+b=complex(input("enter a complex"))
+print(a+b)
+'''
+'''
+a=bool(input("enter bool"))
+b=bool(input("enter bool"))
+c=bool(input("enter bool"))
+print(a+b+c)
+'''
+'''a=int(input("enter a value"))
+b=int(input("enter b value"))
+option=int(input("1 add 2 sub 3 mul"))
+print(a+b)
+print(a-b)
+print(a*b)
+'''
+'''
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+option=input("1 add 2 sub 3 mul")
+print(a+b)
+print(a-b)
+print(a*b)
+
+a=int(input())
+b=int(input())
+print(a+b)
+'''
+'''
+name=input()
+print(name)
+'''
+'''
+# swapping no's without using runtime input
+a=10;b=20
+#1
+a,b=b,a
+# 2
+temp=0
+temp=a
+a=b
+b=temp
+# 3
+a=a+b
+b=a-b
+a=a-b
+# 4
+a=a*b
+b=a/b
+a=a/b
+# 5
+print(a,b)
+print(f"b={b} a={a}")
+#6
+print("%d %d" %(a,b))
+'''
+'''
+a=float(input( "enter a" ))
+b=float(input("entre b"))
+a=a+b
+b=a-b
+a=a-b
+print("after swapping",a,b)
+'''
+'''
+a=float(input( "enter a" ))
+b=float(input("entre b"))
+a,b=b,a
+print("after swapping",a,b)
+'''
+'''
+a=float(input( "enter a" ))
+b=float(input("entre b"))
+temp=a
+a=b
+b=temp
+print("after swapping",a,b)
+'''
+'''
+a=float(input( "enter a" ))
+b=float(input("entre b"))
+temp=a
+a=b
+b=temp
+print("after swapping a=%.2f b=%.2f" %(a,b))
+'''
+#swaping strings
+'''
+a=input("str input)
+b=input("str input)
+a,b=b,a
+print(a,b)
+'''
+'''
+a=(input( "enter a string" ))
+b=(input("entre a string"))
+temp=a
+a=b
+b=temp
+print("after swapping a=%s b=%s" %(a,b))
+'''
